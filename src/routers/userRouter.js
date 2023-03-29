2a8aba51eab5a7c261ba17cd95e5edd934d24ef2
Batrawi <batrawi2000forever@gmail.com>
2023-03-29 09:42:36 +0300

    fix router relates #3

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const router = express();
-const { notFoundError, serverError} =require("../controllers/errorController");
+
+const router = express.Router();
+const { notFoundError, serverError } = require('../controllers/errorController');
+
 router.use(notFoundError);
 router.use(serverError);
