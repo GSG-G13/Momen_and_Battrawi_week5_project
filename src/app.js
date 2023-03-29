5badc62c50f0e44ae16529d63b3bcab13991f4bc
Momen Qudaih <momenqudaih1@gmail.com>
2023-03-29 11:37:34 +0200

    fix structure v2 relates #14 closes #14

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-// here static files
+// This is all hopefully familiar by now
+const express = require('express');
+
+const routes = require('./routers');
+
+const app = express();
+
+// Parses the request payload (if JSON) before the request goes to the handler
+app.use(express.json());
+
+app.use(routes);
+
+module.exports = app;
